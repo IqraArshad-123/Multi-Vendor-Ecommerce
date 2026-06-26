196576d051d630605396353da9edab1e6ada6e8e
IqraArshad-123 <iqraa6244@gmail.com>
2026-06-26 20:13:17 +0500

    Deploying backend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,2 @@
 export const server = "http://localhost:8000";
-
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://multi-vendor-ecommerce-production-5522.up.railway.app/";
