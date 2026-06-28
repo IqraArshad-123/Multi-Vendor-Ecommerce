c74dbcb485bc5353caedc168c49d5e8c99030f6c
IqraArshad-123 <iqraa6244@gmail.com>
2026-06-29 00:13:27 +0500

    fix: update Railway URL and CORS

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "https://multi-vendor-ecommerce-production-5522.up.railway.app";
-export const backend_url = "https://multi-vendor-ecommerce-production-5522.up.railway.app/";
+export const server = "https://multi-vendor-ecommerce-production-29ea.up.railway.app";
+export const backend_url = "https://multi-vendor-ecommerce-production-29ea.up.railway.app/";
